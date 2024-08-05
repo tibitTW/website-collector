bc9dc0f6040b652277990f3d5476da18f8efc5bc
tibitTW <chunhua.2147483647@gmail.com>
2024-08-05 15:17:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+    <div class="container">
+        <div class="sidebar"></div>
+        <div class="main"></div>
+    </div>
+</template>
+
+<style scoped>
+container {
+    display: flex;
+}
+</style>
