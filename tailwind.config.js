c6608d257b6e78599db9d50d7a77c40ffed4f533
tibitTW <chunhua.2147483647@gmail.com>
2024-08-19 14:24:05 +0800

    feat: add global tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
-const { addIconSelectors } = require("@iconify/tailwind");
+const { addIconSelectors } = require('@iconify/tailwind')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    content: [
+        './src/*.html',
+        './src/**/*.{vue,js,ts,jsx,tsx}',
+        './src/**/*.css',
+    ],
     theme: {
         extend: {},
     },
     plugins: [
         // Iconify plugin, requires writing list of icon sets to load
-        addIconSelectors({ prefixes: ["lucide"], scale: 1 }),
+        addIconSelectors({ prefixes: ['lucide'], scale: 1 }),
+        function ({ addUtilities }) {
+            addUtilities({
+                '.flex-center': {
+                    display: 'flex',
+                    'align-items': 'center',
+                    'justify-content': 'center',
+                },
+            })
+        },
     ],
-};
+}
